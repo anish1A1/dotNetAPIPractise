e1a0f74823a2e607bb0c4cd901562f03adcc3b49
anish1A1 <alloozpat87@gmail.com>
2025-03-10 09:50:03 +0545

    first commit

---
+++ Controllers/fruits.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace learningAPI.Controllers
+{
+    [Route("api/fruits")]
+    [ApiController]
+    public class fruits : ControllerBase
+    {
+    }
+}
